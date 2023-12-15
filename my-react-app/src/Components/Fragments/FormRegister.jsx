899488747b3f3a4dfa3618febcbfe66666d4f571
toomiinn <butouchiha9@gmail.com>
2023-12-16 00:11:34 +0700

    Update FormRegister.jsx

--- my-react-app/src/Components/Fragments/FormRegister.jsx
+++ my-react-app/src/Components/Fragments/FormRegister.jsx
@@ -7,7 +7,7 @@ const FormRegister = () => {
       <InputForm label="Fullname" type="text" placeholder="insert your name here..." name="fullname" />
       <InputForm label="Email" type="email" placeholder="example@mail.com" name="email" />
       <InputForm label="Password" type="password" placeholder="******" name="password" />
-      <InputForm label="Confirm Password" type="password" placeholder="ConfirmPassword" name="email" />
+      <InputForm label="Confirm Password" type="password" placeholder="ConfirmPassword" name="confirmpassword" />
       <Button classname="bg-blue-600 w-full">Register</Button>
     </form>
   );
