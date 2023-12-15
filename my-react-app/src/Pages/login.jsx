9a9d092a88e3a2b006008e6a54e50d3ee405112a
toomiinn <butouchiha9@gmail.com>
2023-12-16 00:11:47 +0700

    Update login.jsx

--- my-react-app/src/Pages/login.jsx
+++ my-react-app/src/Pages/login.jsx
@@ -1,10 +1,17 @@
 import AuthLayout from "../Components/Layouts/AuthLayouts";
 import FormLogin from "../Components/Fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <AuthLayout title="Login">
       <FormLogin />
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?{" "}
+        <Link to="/register" className="font-bold text-blue-600">
+          Sign up
+        </Link>
+      </p>
     </AuthLayout>
   );
 };
