39d22897640a1596c14c0a0d03e5b1db2455d2e4
toomiinn <butouchiha9@gmail.com>
2023-12-23 15:18:26 +0700

    Update CardProduct.jsx

--- my-react-app/src/Components/Fragments/CardProduct.jsx
+++ my-react-app/src/Components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button/Index";
 
 const CardProduct = (props) => {
   const { children } = props;
-  return <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow">{children}</div>;
+  return <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow mx-2 my-2 flex flex-col justify-between">{children}</div>;
 };
 
 const Header = (props) => {
@@ -15,11 +15,11 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-  const { children, title } = props;
+  const { children, name } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
-        <h5 className="text-xl font-semibold tracking-tight text-white">{title}</h5>
+        <h5 className="text-xl font-semibold tracking-tight text-white">{name}</h5>
         <p className="text-m text-white">{children}</p>
       </a>
     </div>
@@ -30,7 +30,7 @@ const Footer = (props) => {
   const { price } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-xl font-bold text-white">{price}</span>
+      <span className="text-xl font-bold text-white">Rp {price.toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</span>
       <Button classname="bg-blue-600">Add To Card</Button>
     </div>
   );
